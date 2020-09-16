67bcd58da8259e375d1f4b7e79d02c7a0b992ee4
Yuri Guller <gullerya@gmail.com>
2020-09-16 14:27:09 +0300

    release design tokens

--- release.sh
+++ release.sh
@@ -1,7 +1,6 @@
 git add --all
 git commit -m 'release design tokens'
 git push
-git tag vRelease
-git push --tags
+git push origin vRelease
+sleep 3
 git push --delete origin vRelease
-git fetch -p -P
