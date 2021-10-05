98ff3222bb0c8f3de7c0d45e67976edfde68cb84
yonatankra <yonatan.kra@vonage.com>
2021-10-05 19:32:20 +0300

    Styling

--- test/test-dictionary-coherent.js
+++ test/test-dictionary-coherent.js
@@ -6,37 +6,37 @@ testSchemes();
 testTypography();
 
 function testSchemes() {
-	for (const scheme of ['dark', 'light']) {
-		for (const alt of ['alternate', 'main']) {
-			console.log(`${os.EOL}testing scheme '${scheme}:${alt}'...`);
-			try {
-				const config = getStyleDictionaryConfig([
-					`./globals/color/**/*.json`,
-					`./schemes/${scheme}/${alt}.json`
-				], 'schemes.scss');
-				StyleDictionaryPackage.extend(config).buildPlatform('web');
-				console.log('... PASSED');
-			} catch (e) {
-				console.log('... FAILED');
-				console.error(e);
-				process.exit(-1);
-			}
-		}
-	}
+    for (const scheme of ['dark', 'light']) {
+        for (const alt of ['alternate', 'main']) {
+            console.log(`${os.EOL}testing scheme '${scheme}:${alt}'...`);
+            try {
+                const config = getStyleDictionaryConfig([
+                    `./globals/color/**/*.json`,
+                    `./schemes/${scheme}/${alt}.json`
+                ], 'schemes.scss');
+                StyleDictionaryPackage.extend(config).buildPlatform('web');
+                console.log('... PASSED');
+            } catch (e) {
+                console.log('... FAILED');
+                console.error(e);
+                process.exit(-1);
+            }
+        }
+    }
 }
 
 function testTypography() {
-	try {
-		console.log(`${os.EOL}testing typography...`);
-		const config = getStyleDictionaryConfig([
-			'./globals/typography/**/*.json',
-			'./typography/**/*.json'
-		], 'typography.scss');
-		StyleDictionaryPackage.extend(config).buildPlatform('web');
-		console.log('... PASSED');
-	} catch (e) {
-		console.log('... FAILED');
-		console.error(e);
-		process.exit(-1);
-	}
+    try {
+        console.log(`${os.EOL}testing typography...`);
+        const config = getStyleDictionaryConfig([
+            './globals/typography/**/*.json',
+            './typography/**/*.json'
+        ], 'typography.scss');
+        StyleDictionaryPackage.extend(config).buildPlatform('web');
+        console.log('... PASSED');
+    } catch (e) {
+        console.log('... FAILED');
+        console.error(e);
+        process.exit(-1);
+    }
 }
