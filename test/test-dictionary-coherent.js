2681efaf5e0296ab082c8d8c5b074e0ea9472a9a
yonatankra <yonatan.kra@vonage.com>
2021-10-14 10:06:32 +0300

    Remove scheme test (done in e2e test)

--- test/test-dictionary-coherent.js
+++ test/test-dictionary-coherent.js
@@ -2,28 +2,7 @@ import os from 'os';
 import StyleDictionaryPackage from 'style-dictionary';
 import {getStyleDictionaryConfig} from "./utils.js";
 
-testSchemes();
 testTypography();
-
-function testSchemes() {
-    for (const scheme of ['dark', 'light']) {
-        for (const alt of ['alternate', 'main']) {
-            console.log(`${os.EOL}testing scheme '${scheme}:${alt}'...`);
-            try {
-                const config = getStyleDictionaryConfig([
-                    `./globals/color/**/*.json`,
-                    `./schemes/${scheme}/${alt}.json`
-                ], 'schemes.scss');
-                StyleDictionaryPackage.extend(config).buildPlatform('web');
-                console.log('... PASSED');
-            } catch (e) {
-                console.log('... FAILED');
-                console.error(e);
-                process.exit(-1);
-            }
-        }
-    }
-}
 
 function testTypography() {
     try {
