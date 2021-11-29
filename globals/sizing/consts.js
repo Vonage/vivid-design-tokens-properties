0b1dba5057be11165fd6143ad36006ec782f9221
Rina <rina.oksman@vonage.com>
2021-11-29 10:36:12 +0200

    remove mapping from consts

--- globals/sizing/consts.js
+++ globals/sizing/consts.js
@@ -1,3 +1,3 @@
 export const SAVE_PATH = 'dist/sizing/sizing.json';
-export const subItems = ['4px-scale', '8px-scale'].map(val => `${val}`);
-export const properties = ['xxxs', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'].map(val => `${val}`);
+export const subItems = ['4px-scale', '8px-scale'];
+export const properties = ['xxxs', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'];
