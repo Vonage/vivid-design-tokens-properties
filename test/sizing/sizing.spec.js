c9199967cba51373741ac3baee74747d2abfb54b
Rina <rina.oksman@vonage.com>
2021-12-01 17:57:19 +0200

    update test

--- test/sizing/sizing.spec.js
+++ test/sizing/sizing.spec.js
@@ -9,20 +9,19 @@ describe(`Sizing Tokens`, function () {
 
     describe(`Figma Parser`, function () {
         let results = [];
-
-        function mockWriteJson(data, path) {
-            results.push({
-                data, path
-            });
-        }
+        let mockWriteJson;
 
         beforeEach(function () {
             results.length = 0;
+            mockWriteJson = jest.fn(function (data, path) {
+                results.push({
+                    data, path
+                });
+            });
         });
 
         describe(`Sizing`, function () {
             it(`should write to JSON only once`, function () {
-                mockWriteJson = jest.fn(mockWriteJson);
                 sizingParser.parse(rawData, mockWriteJson);
                 expect(mockWriteJson.mock.calls).toHaveLength(1);
             });
@@ -34,7 +33,7 @@ describe(`Sizing Tokens`, function () {
 
             it(`should generate the correct sizing values json from Figma`, function () {
                 sizingParser.parse(rawData, mockWriteJson);
-                expect({...expectedResult}).toEqual(results[0].data);
+                expect({ ...expectedResult }).toEqual(results[0].data);
             });
 
             it(`should save to correct files paths`, function () {
